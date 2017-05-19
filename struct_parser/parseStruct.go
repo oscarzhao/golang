de4515a828603ebcd6dbe0f5b97ffff49b377700
oscarzhao <oscarzhaosl@gmail.com>
2017-05-19 15:42:17 +0800

    Add support to nested structs, arrays and fundamental types

--- struct_parser/parseStruct.go
+++ struct_parser/parseStruct.go
@@ -2,42 +2,136 @@ package structparser
 
 import (
 	"fmt"
+	"log"
 	"reflect"
 	"strings"
+	"time"
 )
+
+// parse time for convenient
+var (
+	timeType    = reflect.TypeOf(time.Time{})
+	timePtrType = reflect.TypeOf(&time.Time{})
+)
+
+// ParserFunc defines the signature of a parser
+type ParserFunc func(reflect.Type) (*Field, error)
 
 // Field is used to store the information of a struct field
 type Field struct {
 	Name        string
+	Kind        string
 	Type        string
 	JSONName    string
 	Required    bool
 	Description string
+
+	// if the Field is a struct, store all its fields in the list
+	// if the Field is an array, store its field in the first element of the list
+	Fields []Field
 }
 
-// ParseStructInfo parses a certain pointer value and return its swagger info
+// Parse a type
+func Parse(typ reflect.Type) (*Field, error) {
+	originTyp := typ
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	parser, ok := getParser(typ)
+	if !ok {
+		log.Printf("Parse, cannot find parser; got %s", originTyp)
+		return nil, fmt.Errorf("cannot find parser func; got %s", originTyp)
+	}
+	return parser(typ)
+}
+
+func getParser(typ reflect.Type) (ParserFunc, bool) {
+	kind := typ.Kind()
+	if kind == reflect.Ptr {
+		kind = typ.Elem().Kind()
+	}
+	switch kind {
+	case reflect.Bool:
+		return parseSimpleType, true
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return parseSimpleType, true
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return parseSimpleType, true
+	case reflect.Float32, reflect.Float64:
+		return parseSimpleType, true
+	case reflect.String:
+		return parseSimpleType, true
+	case reflect.Array, reflect.Slice:
+		return parseArrayType, true
+	case reflect.Struct:
+		return parseStructType, true
+	default:
+		return nil, false
+	}
+}
+
+// parseSimpleType parses a certain pointer value and return its type
 // pointer is ignored
-func ParseStructInfo(ifc interface{}) ([]Field, error) {
-	typ := reflect.TypeOf(ifc)
-	if typ.Kind() != reflect.Ptr || typ.Elem().Kind() != reflect.Struct {
-		return nil, fmt.Errorf("dest must be pointer to struct; got %T", typ)
+func parseSimpleType(typ reflect.Type) (*Field, error) {
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	if typ.Kind() > reflect.Float64 && typ.Kind() != reflect.String {
+		return nil, fmt.Errorf("dest must be a (ptr to) simple type; got %s", typ)
+	}
+	info := Field{Kind: typ.Kind().String(), Type: typ.String()}
+	return &info, nil
+}
+
+// parseArrayType parses a certain pointer value and return its type
+// pointer is ignored
+func parseArrayType(typ reflect.Type) (*Field, error) {
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	if typ.Kind() != reflect.Array && typ.Kind() != reflect.Slice {
+		return nil, fmt.Errorf("dest must be a (ptr to) array/slice type; got %s", typ)
+	}
+	info := Field{Kind: typ.Kind().String(), Type: typ.String()}
+	elemType := typ.Elem()
+	parser, ok := getParser(elemType)
+	if !ok {
+		return nil, fmt.Errorf("cannot find parser for field; got %s", elemType)
+	}
+	subInfo, err := parser(elemType)
+	if err != nil {
+		return nil, err
+	}
+	info.Fields = append(info.Fields, *subInfo)
+	return &info, nil
+}
+
+// parseStructType parses a certain pointer value and return its swagger info
+// pointer is ignored
+func parseStructType(typ reflect.Type) (*Field, error) {
+	originTyp := typ
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	if typ.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("dest must be a (ptr to) array/slice type; got %s", originTyp.Kind())
 	}
 	var infos []Field
-	elem := typ.Elem()
-	fieldCount := elem.NumField()
+	fieldCount := typ.NumField()
 	for i := 0; i < fieldCount; i++ {
-		field := elem.Field(i)
+		field := typ.Field(i)
 
 		var info Field
 		info.Name = field.Name
 		info.JSONName = field.Tag.Get("json")
 
 		if field.Type.Kind() == reflect.Ptr {
-			info.Type = field.Type.Elem().String()
+			info.Kind = field.Type.Elem().Kind().String()
 		} else {
-			info.Type = field.Type.String()
+			info.Kind = field.Type.Kind().String()
 		}
 
+		// calculate swagger info
 		swaggerTag := field.Tag.Get("swagger")
 		items := strings.Split(swaggerTag, ",")
 		if len(items) < 2 {
@@ -51,7 +145,32 @@ func ParseStructInfo(ifc interface{}) ([]Field, error) {
 			return nil, fmt.Errorf("field:%s, invalid swagger tag: %s", field.Name, swaggerTag)
 		}
 		info.Description = strings.Trim(items[1], " ")
+
+		// filter out time.Time
+		if field.Type == timePtrType || field.Type == timeType {
+			info.Type = timeType.String()
+			infos = append(infos, info)
+			continue
+		}
+
+		// parse type information and nested structure
+		parser, ok := getParser(field.Type)
+		if !ok {
+			return nil, fmt.Errorf("cannot find parser for field %s; got %s", field.Name, field.Type)
+		}
+		typeInfo, err := parser(field.Type)
+		if err != nil {
+			fmt.Printf("parse field %s failed, type:%s, err:%s\n", field.Name, field.Type, err)
+			return nil, err
+		}
+		info.Type = typeInfo.Type
+		info.Fields = typeInfo.Fields
+
 		infos = append(infos, info)
 	}
-	return infos, nil
+	info := &Field{
+		Type:   typ.Name(),
+		Fields: infos,
+	}
+	return info, nil
 }
--- struct_parser/parseStruct_test.go
+++ struct_parser/parseStruct_test.go
@@ -2,28 +2,54 @@ package structparser
 
 import (
 	"testing"
+	"time"
+
+	"reflect"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParseStructTag(t *testing.T) {
+	type Crime struct {
+		Time      time.Time `json:"time" swagger:"Required, the time crime is committed"`
+		Place     string    `json:"place" swagger:"Required, the place where crime is committed"`
+		Serverity int8      `json:"serverity" swagger:"Required, the serverity of the crime"`
+	}
 	// Person ...
 	type Person struct {
-		ID      int64   `json:"id" swagger:"Required, ID of the person"`
-		Name    string  `json:"name" swagger:"Required, Name of the person"`
-		Age     int     `json:"age" swagger:"Optional, Age of the person"`
-		Hobbies *string `json:"hobbies" swagger:"Optional, Hobbies of the person"`
+		ID           int64     `json:"id" swagger:"Required, ID of the person"`
+		Name         string    `json:"name" swagger:"Required, Name of the person"`
+		Age          int       `json:"age" swagger:"Optional, Age of the person"`
+		Hobbies      *string   `json:"hobbies" swagger:"Optional, Hobbies of the person"`
+		CrimeHistory []Crime   `json:"crimeHistory" swagger:"Optional, Crime history of the person"`
+		Created      time.Time `json:"created" swagger:"Required, Created time of the record"`
 	}
-	p := &Person{
-		ID:   1,
-		Name: "oscar",
-		Age:  22,
-	}
-	fields, err := ParseStructInfo(p)
+	p := &Person{}
+	field, err := Parse(reflect.TypeOf(p))
 	assert.Nil(t, err, "parse Person struct should succeed")
-	assert.Equal(t, 4, len(fields), "check struct Person's field number")
-	assert.Equal(t, "int64", fields[0].Type)
-	assert.Equal(t, "string", fields[1].Type)
-	assert.Equal(t, "int", fields[2].Type)
-	assert.Equal(t, "string", fields[3].Type)
+	assert.Equal(t, 6, len(field.Fields), "check struct Person's field number")
+
+	assert.Equal(t, "int64", field.Fields[0].Kind, "test ID")
+	assert.Equal(t, "int64", field.Fields[0].Type, "test ID")
+	assert.Equal(t, "id", field.Fields[0].JSONName, "test ID")
+
+	assert.Equal(t, "string", field.Fields[1].Kind, "test Name")
+	assert.Equal(t, "string", field.Fields[1].Type, "test Name")
+	assert.Equal(t, "name", field.Fields[1].JSONName, "test Name")
+
+	assert.Equal(t, "int", field.Fields[2].Kind, "test Age")
+	assert.Equal(t, "int", field.Fields[2].Type, "test Age")
+	assert.Equal(t, "age", field.Fields[2].JSONName, "test Age")
+
+	assert.Equal(t, "string", field.Fields[3].Kind, "test Hobbies")
+	assert.Equal(t, "string", field.Fields[3].Type, "test Hobbies")
+	assert.Equal(t, "hobbies", field.Fields[3].JSONName, "test Hobbies")
+
+	assert.Equal(t, "slice", field.Fields[4].Kind, "test CrimeHistory")
+	assert.Equal(t, "[]structparser.Crime", field.Fields[4].Type, "test CrimeHistory")
+	assert.Equal(t, "crimeHistory", field.Fields[4].JSONName, "test CrimeHistory")
+
+	assert.Equal(t, "struct", field.Fields[5].Kind, "test Created")
+	assert.Equal(t, "time.Time", field.Fields[5].Type, "test Created")
+	assert.Equal(t, "created", field.Fields[5].JSONName, "test Created")
 }
