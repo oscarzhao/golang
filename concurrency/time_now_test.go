fdd0bfb77a5e3300ca8d3d2fca863b01bfe5153c
oscarzhao <oscarzhaosl@gmail.com>
2018-03-27 18:49:11 +0800

    Benchmark test for time.Now()

---
+++ concurrency/time_now_test.go
@@ -0,0 +1,16 @@
+package concurrency
+
+import (
+	"testing"
+	"time"
+)
+
+// under linux, expected 40-50 ns/op
+// under macos, expected 700-800 ns/op
+func BenchmarkTimeNow(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = time.Now()
+	}
+}
+
+// run with `go test --bench=.`
